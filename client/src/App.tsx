36e8964a5273d2b16affb53694e0685a32ff3df6
galobponce <personal.galo.bello@gmail.com>
2022-05-23 15:07:11 -0500

    agrego cliente vacio

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+
+const App: FC = () => {
+  return (
+    <div className="app-container">
+      <h3>Soon</h3>
+      <h5>VideoChat with everyone!</h5>
+    </div>
+  );
+}
+
+export default App;
